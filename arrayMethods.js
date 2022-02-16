8877f6d71f092f93cfbac2f9da1c8fc855c2d025
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-16 11:49:12 -0700

    Array Methods problem 2 solve

--- arrayMethods.js
+++ arrayMethods.js
@@ -41,7 +41,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map((price) => Number((price * 1.07).toFixed(2)));
+console.log(postTaxPrices);
 
 
 
--- check.js
+++ check.js
@@ -20,3 +20,23 @@ const mixedNumbers = [6, 3, 1, 7, 5, 2, 6, 8, 9, 4, 2, 7, 9, 3, 1, 8, 4, 3];
 // CODE HERE
 const evenNumbers = mixedNumbers.filter((number) => number % 2 === 0);
 console.log(evenNumbers);
+
+////////// PROBLEM 2 //////////
+
+// Do not edit the code below.
+const prices = [15.0, 23.0, 78.0, 34.0, 12.0, 86.0, 12.0, 79.0, 32.0];
+// Do not edit the code above.
+
+/*
+  Use the map method on the prices array to calculate a new array of post-tax prices.
+  Use a 7% tax rate.
+  Math reminder! To calculate the price after tax, multiply the price by 1 plus the taxRate as a decimal.
+  Example: if tax is 7%, the price afterTax could be calculated like this: afterTax = price * 1.07) 
+  The map function also takes a callback with the parameters
+  function(element, index, wholeArray){}  Function Form
+  (element, index, wholeArray)=>{}    Arrow Form
+*/
+
+// CODE HERE
+const postTaxPrices = prices.map((price) => Number((price * 1.07).toFixed(2)));
+console.log(postTaxPrices);
